15a8309ab3e2bba28a3b06a45b45655e956ebc3b
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-05-26 23:35:58 -0400

    Refactored index.html, finished styling, finished question array. Will need to refactor functions to evaluate user answer and update new questions.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,56 +11,84 @@ var quizCard= document.getElementById("quiz-card")
 
 
 var questions = [ {
-  number:1,
+  answer:1,
   question:'Who invented JavaScript?',
-  Answer1:'Netscape', 
-  Answer2: 'AOL',
-  Answer3:'Microsoft',
-  Answer4:'Nomadic Merchants from Tatoine'
+  option1:'Netscape', 
+  option2: 'AOL',
+  option3:'Microsoft',
+  option4:'Nomadic Merchants from Tatoine'
 },{
-  number:2,
+  answer:2,
   question: 'What method would you use to remove the last element from an array?',
-  Answer1:'.fitler()',
-  Answer2: '.pop()',
-  Answer3:'.map()',
-  Answer4:'.getouttahere()'
+  option1:'.fitler()',
+  option2: '.pop()',
+  option3:'.map()',
+  option4:'.getouttahere()'
 },{
-  number:3,
+  answer:4,
   question: 'Which of the following is an example of a semantic HTML tag?',
-  Answer1:'<p>',
-  Answer2: '<div>',
-  Answer3:'<semantic>',
-  Answer4:'<footer>'
+  option1:'<p>',
+  option2: '<div>',
+  option3:'<semantic>',
+  option4:'<footer>'
 },{
-  number:4,
+  answer:2,
   question: 'Which code block would change the text color red for an HTML element with a "change-me" class?',
-  Answer1:'#change-me{color:red;}',
-  Answer2: '.change-me{color:red;}',
-  Answer3:'change-me{text-color:red;}',
-  Answer4:'.change-me{just turn red please!}'
+  option1:'#change-me{color:red;}',
+  option2: '.change-me{color:red;}',
+  option3:'change-me{text-color:red;}',
+  option4:'.change-me{just turn red please!}'
 },{
-  number:5,
+  answer:4,
   question: 'Which of the following is strict comparison?',
-  Answer1:'!=',
-  Answer2: '=',
-  Answer3:'==',
-  Answer4:'==='
+  option1:'!=',
+  option2: '=',
+  option3:'==',
+  option4:'==='
 
-}/*,{
-  number:6,
-  question: 'Which of the following is strict comparison?',
-  Answer1:'!=',
-  Answer2: '=',
-  Answer3:'==',
-  Answer4:'==='
-},{},{},{},{}*/]
+},{
+  answer: 3,
+  question: 'Which selection will log something to the console',
+  option1:'console.log',
+  option2: 'log.console()',
+  option3:'console.log()',
+  option4:'Abracadabra.Shout()'
+},{
+  answer: 1,
+  question: 'Which is a proper function?',
+  option1:'function(x){doSomething;};',
+  option2: 'function{x}(doSomething;);',
+  option3:'function[x][doSomething;];',
+  option4:'function.dosomething()'
+},{
+  answer: 4,
+  question: 'Which is not a JavaScript Data Type?',
+  option1:'Boolean',
+  option2: 'Number',
+  option3:'String',
+  option4:'Thread'
+},{
+  answer: 2,
+  question: 'What does DOM stand for?',
+  option1:'Document Object Mode',
+  option2: 'Design Object Model',
+  option3:'Document Object Model',
+  option4:'Dominoes Order Meal'
+},{
+  answer: 1,
+  question: 'What is this object type? var num =[1,2,3,4]',
+  option1:'Array',
+  option2: 'Attribute',
+  option3:'Beret',
+  option4:'Function'
+}]
 
 
 
 
 function countdown(){
   
-  var timeLeft = 60;
+  var timeLeft = 10;
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function(){
@@ -82,19 +110,19 @@ function countdown(){
   
     function startQuiz(){
       questionTitle.textContent = questions[0].question;
-      option1.textContent= questions[0].Answer1;
-      option2.textContent=questions[0].Answer2;
-      option3.textContent=questions[0].Answer3;
-      option4.textContent=questions[0].Answer4;
+      option1.textContent= questions[0].option1;
+      option2.textContent=questions[0].option2;
+      option3.textContent=questions[0].option3;
+      option4.textContent=questions[0].option4;
       start.setAttribute("style", "display:none")
     }   
-  /*need a click event listener that evaluates the user response that executes a function, this fuction will determine if the answer is true or false, and exectute the next question function*/ 
+  /*need a click event listener that evaluates the user response that executes a function, this fuction will determine if the option is true or false, and exectute the next question function*/ 
 function nextQuestion(){
   questionTitle.textContent = questions[index++].question;
-  option1.textContent=question[index++].Answer1;
-  option2.textContent=question[index++].Answer2;
-  option3.textContent=question[index++].Answer3;
-  option4.textContent=question[index++].Answer4;
+  option1.textContent=question[index++].option1;
+  option2.textContent=question[index++].option2;
+  option3.textContent=question[index++].option3;
+  option4.textContent=question[index++].option4;
 } 
 
 option1.addEventListener('click',function(){
