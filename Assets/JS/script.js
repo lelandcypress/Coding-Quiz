af68eb89fca30c01b4725f296df4d5f5bb5b84eb
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-05-27 23:12:32 -0400

    added Bootstrap styling

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -13,80 +13,8 @@ var score= 0;
 var remainingQuestions = []
 var currentQuestion = {}
 
-var questions = [ {
-  answer:1,
-  question:'Who invented JavaScript?',
-  option1:'Netscape', 
-  option2: 'AOL',
-  option3:'Microsoft',
-  option4:'Nomadic Merchants from Tatoine'
-},{
-  answer:2,
-  question: 'What method would you use to remove the last element from an array?',
-  option1:'.fitler()',
-  option2: '.pop()',
-  option3:'.map()',
-  option4:'.getouttahere()'
-},{
-  answer:4,
-  question: 'Which of the following is an example of a semantic HTML tag?',
-  option1:'<p>',
-  option2: '<div>',
-  option3:'<semantic>',
-  option4:'<footer>'
-},{
-  answer:2,
-  question: 'Which code block would change the text color red for an HTML element with a "change-me" class?',
-  option1:'#change-me{color:red;}',
-  option2: '.change-me{color:red;}',
-  option3:'change-me{text-color:red;}',
-  option4:'.change-me{just turn red please!}'
-},{
-  answer:4,
-  question: 'Which of the following is strict comparison?',
-  option1:'!=',
-  option2: '=',
-  option3:'==',
-  option4:'==='
 
-},{
-  answer: 3,
-  question: 'Which selection will log something to the console',
-  option1:'console.log',
-  option2: 'log.console()',
-  option3:'console.log()',
-  option4:'Abracadabra.Shout()'
-},{
-  answer: 1,
-  question: 'Which is a proper function?',
-  option1:'function(x){doSomething;};',
-  option2: 'function{x}(doSomething;);',
-  option3:'function[x][doSomething;];',
-  option4:'function.dosomething()'
-},{
-  answer: 4,
-  question: 'Which is not a JavaScript Data Type?',
-  option1:'Boolean',
-  option2: 'Number',
-  option3:'String',
-  option4:'Thread'
-},{
-  answer: 2,
-  question: 'What does DOM stand for?',
-  option1:'Document Object Mode',
-  option2: 'Design Object Model',
-  option3:'Document Object Model',
-  option4:'Dominoes Order Meal'
-},{
-  answer: 1,
-  question: 'What is this object type? var num =[1,2,3,4]',
-  option1:'Array',
-  option2: 'Attribute',
-  option3:'Beret',
-  option4:'Function'
-}]
-
-var timeLeft = 30
+var timeLeft = 5
 
 
 function countdown(){
@@ -102,7 +30,7 @@ function countdown(){
       --timeLeft;
     } else {
       clearInterval(timeInterval);
-      timerEl.textContent = '';
+      timerEl.textContent = 'Sorry! Out of Time';
       gameOver();  
       }
 
@@ -187,12 +115,12 @@ function correct(){
 }
 
 function gameOver(){
-    messageEL.textContent= 'Sorry! Out of Time';
+   ;
 }
 
 start.addEventListener("click",function(){
     countdown()
     startQuiz()
-    quizCard.style.display= 'block';
+   
 })
 
