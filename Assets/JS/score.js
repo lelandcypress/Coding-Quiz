a8b5feb14d466cd310f2d10252fb32f11c12a60f
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-05-28 23:42:55 -0400

    completed local storage. All Scores are displaying and persisting as expected. Need to add a functioning 'go back' button, and finish styling

--- Assets/JS/score.js
+++ Assets/JS/score.js
@@ -1,4 +1,25 @@
-var userSubmission;
+var userSubmission = document.getElementById("entername");
+var subButton = document.getElementById("submit");
 var displayScore = document.getElementById("the-Score");
 var getScore = localStorage.getItem("coding quiz score");
+var hidden = document.getElementById("hidden-table");
+
 displayScore.textContent = getScore;
+subButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var scoreTracker = {
+    name: userSubmission.value,
+    score: getScore,
+  };
+  localStorage.setItem("scoreTracker", JSON.stringify(scoreTracker));
+  renderMessage();
+});
+function renderMessage() {
+  var lastScore = JSON.parse(localStorage.getItem("scoreTracker"));
+  var p = document.createElement("p");
+  var scoreRow = hidden.appendChild(p);
+  scoreRow.textContent = lastScore.name + " " + lastScore.score;
+}
+
+renderMessage();
