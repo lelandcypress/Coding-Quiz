81f7518519cee19feebcac41de7bed028a3f0c1d
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-05-31 20:22:50 -0400

    Application meets AC and is ready for deployment

--- Assets/JS/score.js
+++ Assets/JS/score.js
@@ -1,19 +1,16 @@
 var userSubmission = document.getElementById("entername");
 var subButton = document.getElementById("submit");
 var tryAgain = document.getElementById("Go-Back");
-var displayScore = document.getElementById("the-Score");
-var getScore = localStorage.getItem("coding quiz score");
-var highScores = document.getElementById("high-scores");
+var scoreList = document.getElementById("score-list");
+var lastScore = JSON.parse(localStorage.getItem("scoreBoard"));
 
-function renderMessage() {
-  var lastScore = JSON.parse(localStorage.getItem("scoreBoard"));
-  var p = document.createElement("p");
-  var scoreRow = highScores.appendChild(p);
-  scoreRow.textContent =
-    lastScore.name + " Scored " + lastScore.score + " Points";
-}
-
-renderMessage();
+//credit: Michael Karen https://michael-karen.medium.com/how-to-save-high-scores-in-local-storage-7860baca9d68
+scoreList.innerHTML = lastScore
+  .map(
+    (score) =>
+      `<p class = fs-6 border-bottom>${score.name}  scored  ${score.score} points </p>`
+  )
+  .join("");
 
 tryAgain.addEventListener("click", function () {
   window.open("index.html");
